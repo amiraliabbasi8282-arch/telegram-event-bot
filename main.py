4356d5d705def53b2f2fafe61fc7169bba870bbf
amiraliabbasi8282-arch <amiraliabbasi8282@gmail.com>
2026-02-23 15:59:56 +0330

    main.py

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 
 TOKEN = os.getenv("BOT_TOKEN")
 EVENT_TOPIC_ID = int(os.getenv("EVENT_TOPIC_ID"))
+TALK_TOPIC_ID = int(os.getenv("TALK_TOPIC_ID"))
 
 async def delete_if_not_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.message
     
-    if message and message.message_thread_id == EVENT_TOPIC_ID:
-        user_id = message.from_user.id
-        chat_id = message.chat.id
-        
-        member = await context.bot.get_chat_member(chat_id, user_id)
-        
-        if member.status not in ["administrator", "creator"]:
-            await message.delete()
+    if message:
+        # بررسی اگر پیام داخل تاپیک Event یا Talk بود
+        if message.message_thread_id in [EVENT_TOPIC_ID, TALK_TOPIC_ID]:
+            user_id = message.from_user.id
+            chat_id = message.chat.id
+            
+            member = await context.bot.get_chat_member(chat_id, user_id)
+            
+            if member.status not in ["administrator", "creator"]:
+                await message.delete()
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, delete_if_not_admin))
